986787c17c3902d2addd60809e85a09f778c9a15
Piotr Niziniecki <pniziniecki@gmail.com>
2014-10-06 14:57:26 +0200

    Jslint improvements

--- js/views/Examples/projectBuild.js
+++ js/views/Examples/projectBuild.js
@@ -2,9 +2,10 @@ define([
     'jquery',
     'backbone',
     'text!./tpl/projectBuild.tpl',
+    'rainbow',
     'vendor/language/javascript',
     'vendor/language/html'
-], function ($, Backbone, tpl) {
+], function ($, Backbone, tpl, Rainbow) {
 
     'use strict';
 
