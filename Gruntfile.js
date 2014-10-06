ea56c62153e32bc75a22ffc6cc2f59d6cb6b507e
Piotr Niziniecki <pniziniecki@gmail.com>
2014-10-06 12:48:50 +0200

    Adding basic gruntfile with jslint options

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    var config = {
+        less: {},
+        jslint: {
+            client: {
+                src: ['js/**/*.js'],
+                exclude: ['js/vendor/**/*.js', 'js/build/**/*.js', 'js/r.js'],
+                directives: {
+                    plusplus: true,
+                    regexp: true,
+                    browser: true,
+                    nomen: true,
+                    vars: true,
+                    todo: true,
+                    unparam: true,
+                    predef: ['Hyperion', 'require', 'define', '_']
+                }
+            }
+        }
+    };
+
+    grunt.initConfig(config);
+
+    grunt.loadNpmTasks('grunt-jslint');
+};
