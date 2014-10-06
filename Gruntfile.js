f0128aa1fd1b49561f10b4bfc98cc120fa07eaf5
Kamil Thomas <Kamil.Thomas@sabre.com>
2014-10-06 14:36:38 +0200

    Adding requireJS build and clean job for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,47 @@ module.exports = function (grunt) {
                     predef: ['Hyperion', 'require', 'define', '_']
                 }
             }
+        },
+        clean: {
+            build: ['js/build']
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'js',
+                    dir: 'js/build',
+                    name: 'main',
+                    fileExclusionRegExp: /^css/,
+                    paths: {
+                        jquery: 'vendor/jquery-2.1.1',
+                        underscore: 'vendor/underscore',
+                        backbone: 'vendor/backbone',
+                        text: 'vendor/text',
+                        bootstrap: 'vendor/bootstrap'
+                    },
+                    shim: {
+                        'backbone': {
+                            'deps': ['underscore', 'jquery', 'text', 'bootstrap']
+                        }
+                    },
+                    include: [
+                        'views/Global/header',
+                        'views/Global/error',
+                        'views/Examples/jsonView',
+                        'views/Examples/simpleView',
+                        'views/Examples/main',
+                        'views/Examples/toJson',
+                        'views/Page/about',
+                        'views/Page/main'
+                    ]
+                }
+            }
         }
     };
 
     grunt.initConfig(config);
 
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-jslint');
 };
